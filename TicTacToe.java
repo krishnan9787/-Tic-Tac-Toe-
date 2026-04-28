88d9948ccada96c7b9bd64d3e77bec54362df0ab
krishnan9787 <k65494989@gmail.com>
2026-04-29 01:08:27 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,31 @@
+public class TicTacToe {
+
+    static char[][] board = new char[3][3];
+
+    // Initialize the board with '-'
+    static void initBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    // Print the board
+    static void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    public static void main(String[] args) {
+        initBoard();
+        printBoard();
+    }
+}
